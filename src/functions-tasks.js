41994d144583ee644fc37035095ea96419ae6c4a
Gunsnfnr <a.j.p.010203@gmail.com>
2024-04-10 22:23:12 +0300

    feat: add solution for getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -209,8 +209,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let startVal = startFrom - 1;
+  return function innerFunc() {
+    startVal += 1;
+    return startVal;
+  };
 }
 
 module.exports = {
